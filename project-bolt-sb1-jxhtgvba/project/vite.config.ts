c3ddf77e58b6dd6a905c669f1c18e4438f120ca5
git-shahid26 <102609484+git-shahid26@users.noreply.github.com>
2025-02-07 11:58:11 +0530

    Update vite.config.ts

--- project-bolt-sb1-jxhtgvba/project/vite.config.ts
+++ project-bolt-sb1-jxhtgvba/project/vite.config.ts
@@ -4,12 +4,5 @@ import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
-  },
+  base:"/book-store-management-system",
 });
